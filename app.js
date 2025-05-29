6b1ef771a49f00c82808cdbac97a0622c0daba2a
dayronZ <dayron.delrosal@gmail.com>
2025-05-29 08:54:44 -0500

    Mensaje descriptivo de los cambios

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ async function capturarScreen(){
 
     await pagina.screenshot(
         {
-            path: './screenshots/ejemplo.png',
+            path: `./screenshots/ejemplo-${Date.now()}.png`,
             fullPage: true
         }
     )
@@ -43,4 +43,32 @@ async function capturarScreen(){
     console.log(':::Termine:::')
 
 }
-capturarScreen();
+//capturarScreen();
+
+async function navegarentrepaginas(){
+    console.log(':::Inicio:::')
+    const navegador = await puppeteer.launch({
+        headless: false,
+        slowMo: 400
+    });
+
+    const pagina = await navegador.newPage();
+
+    await pagina.goto('https://example.com/');
+
+    await pagina.click('div>p>a[href="https://www.iana.org/help/example-domains"]');
+
+    await new promise (()=>setTimeout(resolve, 300))
+
+    await pagina.screenshot(
+        {
+            path: "./screenshots/Pagina-visitado-Autom.png",
+            fullPage: true,
+        }
+    )
+    navegador.close();
+
+    console.log(':::Termine:::')
+
+}
+navegarentrepaginas();
